40fba12ac2398cb976e9665258e1b46fd8835303
Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
2024-03-30 00:03:40 +0530

    PAR-8 Refactor App component and add useMovies custom hook (#37)
    * Add keydown event listener to focus input element and clear query state (#23)
    
    Co-authored-by: Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
    
    * Add user rating count to newMovie object
    
    * PAR-5 Add tests for MovieDetails component
    
    * Add test comment for commit
    
    * Remove test comment in Navbar component
    
    * test
    
    * test
    
    * Master -> Dev (#30)
    
    * Add keydown event listener to focus input element and clear query state (#23) (#24)
    
    Co-authored-by: Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
    
    * Update node.js.yml (#25)
    
    Execute Build Deploy only on push or merge to master branch
    
    * PAR-5 Dev (#26)
    
    * Add keydown event listener to focus input element and clear query state (#23)
    
    Co-authored-by: Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
    
    * Add user rating count to newMovie object
    
    * PAR-5 Add tests for MovieDetails component
    
    ---------
    
    Co-authored-by: Nexus <75022247+Nikhil20051523@users.noreply.github.com>
    
    * Jira Issue Check.yml (#27)
    
    * Dev (#28)
    
    * Add keydown event listener to focus input element and clear query state (#23)
    
    Co-authored-by: Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
    
    * Add user rating count to newMovie object
    
    * PAR-5 Add tests for MovieDetails component
    
    * Add test comment for commit
    
    ---------
    
    Co-authored-by: Nexus <75022247+Nikhil20051523@users.noreply.github.com>
    
    * PAR-5 Rename main.yml to jira-issue-check.yml
    
    * Update jira-issue-check.yml
    
    * Update jira-issue-check.yml
    
    * Dev (#29)
    
    * Add keydown event listener to focus input element and clear query state (#23)
    
    Co-authored-by: Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
    
    * Add user rating count to newMovie object
    
    * PAR-5 Add tests for MovieDetails component
    
    * Add test comment for commit
    
    * Remove test comment in Navbar component
    
    * test
    
    * test
    
    ---------
    
    Co-authored-by: Nexus <75022247+Nikhil20051523@users.noreply.github.com>
    
    * Revert "Update jira-issue-check.yml"
    
    This reverts commit 0c9a83ce07f66b470490f1ef617ecfd16085e774.
    
    ---------
    
    Co-authored-by: Nexus <75022247+Nikhil20051523@users.noreply.github.com>
    
    * PAR-5 Remove test comment in Navbar component
    
    * test
    
    * test
    
    * test
    
    * PAR-5 test
    
    * PAR-5 Update jira-issue-check.yml (#35)
    
    * PAR-8 Refactor App component and add useMovies custom hook
    
    * Revert "PAR-8 Refactor App component and add useMovies custom hook"
    
    This reverts commit 794fdcee09960c07abaf778bf436872213ec66c6.
    
    * PAR-8 Add useMovies hook for fetching movie data
    
    ---------
    
    Co-authored-by: Nexus <75022247+Nikhil20051523@users.noreply.github.com>

---
+++ src/useMovies.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+
+const APIKEY = "8c757fb"; // API key for OMDB API
+
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]); // State for storing movie data
+  const [isLoading, setIsLoading] = useState(false); // State for loading status
+  const [error, setError] = useState(null); // State for error handling
+
+  // Fetching Movie Data using useEffect hook
+  useEffect(() => {
+    callback?.();
+    const controller = new AbortController();
+    // Async function to fetch movies
+    async function fetchMovies() {
+      try {
+        setIsLoading(true); // Set loading status to true
+        setError(null); // Clear any previous error
+
+        const res = await fetch(
+          // Fetch movie data from OMDB API
+          `https://www.omdbapi.com/?s=${query.trim()}&apikey=${APIKEY}`,
+          { signal: controller.signal }
+        );
+        const data = await res.json(); // Parse response to JSON
+        // console.log(data);
+        //
+        if (data.Response === "False") {
+          throw new Error(data.Error); // Throw error if response is false
+        }
+
+        setMovies(data.Search); // Set fetched movies to state
+        setIsLoading(false); // Set loading status to false
+      } catch (err) {
+        if (err.message === "Failed to fetch") {
+          setError("Something Went Wrong..."); // Set error message for failed fetch
+          // console.error(err.message);
+        } else if (err.name === "AbortError") {
+          console.log("User Aborted"); // Ignoring the Abort Error
+        } else {
+          setError(err.message); // Set error message for other errors
+        }
+      } finally {
+        setIsLoading(false); // Set loading status to false
+        // setError(null);
+      }
+    }
+    if (query.length < 3) {
+      setMovies([]); // Clear movies data if query length is less than 3
+      setError(null); // Clear any previous error
+      return;
+    }
+    // handleClosebtn();
+    fetchMovies(); // Call fetchMovies function
+
+    return function () {
+      controller.abort();
+    };
+  }, [query]); // useEffect dependency on query state
+
+  return [movies, isLoading, error];
+}
