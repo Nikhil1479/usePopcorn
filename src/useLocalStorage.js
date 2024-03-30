69cd2dadd0f1d4a0348affe8a5571fc7f73eb397
Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
2024-03-30 23:20:27 +0530

    PAR-9 Add useLocalStorage hook to manage watched movies

---
+++ src/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorge(initialValue, key) {
+  const [value, setValue] = useState(function () {
+    const browserData = localStorage.getItem(key);
+    return JSON.parse(browserData) || [];
+  }); // State for storing watched movies
+
+  // Storing the Watched movie data in browser local storage
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
