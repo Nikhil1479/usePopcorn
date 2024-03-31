112d3aa052bfdc0aece60bdef3912f0005b01f4c
Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
2024-04-01 00:37:27 +0530

    PAR-9 Fix initial value in useLocalStorage hook

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 export function useLocalStorge(initialValue, key) {
   const [value, setValue] = useState(function () {
     const browserData = localStorage.getItem(key);
-    return JSON.parse(browserData) || [];
+    return JSON.parse(browserData) || initialValue;
   }); // State for storing watched movies
 
   // Storing the Watched movie data in browser local storage
